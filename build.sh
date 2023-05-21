ba54cc0ae7fb1df68506105c9b83ea194bcac67d
Kyle Stemen <bluelightning32@gmail.com>
2023-05-20 20:22:42 -0700

    Move the original NotationTest into alt

--- build.sh
+++ build.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
+echo "main"
 coq_makefile -o CoqMakefile -f _CoqProject
 make -f CoqMakefile "$@"
+
+if [ "$1" != "install" ]; then
+  (
+    declare -a filtered
+    filtered=()
+    for o in "$@"; do
+      if [ "$o" = "install" ]; then
+        continue;
+      fi
+      filtered+=("$o")
+    done
+    echo "alt"
+    cd alt
+    coq_makefile -o CoqMakefile -f _CoqProject
+    make -f CoqMakefile "$filtered"
+  )
+fi
+
